7e1bf71e55d2b5eb085543cda08d353562d83247
Masha0701 <reflection2008@gmail.com>
2022-11-06 18:15:40 +0300

    feat: copy files ready

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,51 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyFiles(source, target) {
+    fs.copyFile(source, target, fs.constants.COPYFILE_FICLONE, err => {
+        if (err) console.log(err);
+        console.log(`Copied ${source} to ${target}`);
+    })
+}
+
+const copyDir = (src, dest, callback) => {
+    console.log(src, dest);
+    const copy = (copySrc, copyDest) => {
+        fs.readdir(copySrc, (error, list) => {
+            if (error)  {
+                callback(error);
+                return;
+            }
+            
+            list.forEach((item) => {
+                const oldPath = path.resolve(copySrc, item);
+                fs.stat(oldPath, (err, stat) => {
+                    if (err) {
+                        callback(err);
+                    } else{
+
+                        const curSrc = path.resolve(copySrc, item);
+                        const curDest = path.resolve(copyDest, item);
+                        if (stat.isFile()) {
+                            //fs.createReadStream(curSrc).pipe(fs.createWriteStream(curDest))
+                            copyFiles(curSrc, curDest);
+                        } else if (stat.isDirectory()) {
+                            fs.mkdir(curDest, {recursive: true});
+                            copy(curSrc, curDest);
+                        }
+                    }
+                });
+            });
+        });
+    }
+
+    fs.access(dest, err => {
+        if (err) {
+            fs.mkdir(dest, {recursive: true});
+        }
+
+        copy(src, dest);
+    });
+};
+
+copyDir(path.resolve(__dirname, 'files'), path.resolve(__dirname, 'files-copy'));
